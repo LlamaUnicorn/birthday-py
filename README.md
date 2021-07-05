fab1e2f569d737cdddab3820e718e83573f1a1ce
LlamaUnicorn <55321531+LlamaUnicorn@users.noreply.github.com>
2021-07-05 10:11:49 +0000

    Added comments to the birthday.py Added description to README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # This line was added through REPLIT.com
 # brithday-py
 # This line added through PyCharm VCS
+
+This code looks up the JSON file for birthdays and prints a message if anyone has a brithday today.
--- birthday.py
+++ birthday.py
@@ -9,16 +9,19 @@ with open('birthday.json') as f:
     data = json.load(f)
 
 
+# Print to check if JSON loaded alright.
 print(data)
 
-
+# Format to day.month
 dm = time.strftime("%d.%m")
 print(f"Today is", dm)
-# print(type(dm))
 
+# print(type(dm)) to check the type
+
+# Convert to a string
 str_dm = str(dm)
 
-
+# Search the dictionary for 
 for name, age in data.items():
     if age == str_dm:
         print(f"Today is {name}'s birthday!")
